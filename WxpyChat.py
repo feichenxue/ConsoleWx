ce3480d32d58150f61302e6444b3ad1e26e6d681
feichenxue <986024058@qq.com>
2019-04-18 12:26:20 +0800

    update

--- WxpyChat.py
+++ WxpyChat.py
@@ -113,7 +113,7 @@ class ConsoleWx(object):
 
 
     def Receive_one(self, who, datatime):
-        Who = who
+        Who = re.sub(">", "", str(who).split()[1])
         @self.bot.register(Who, except_self=False)
         def print_one_messages(msg):
             print("\n[{} {}@{} ↩ ]".format(datatime, Who, self.myself), msg)
